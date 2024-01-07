2f9abd5f8e363b5d9f85962466bd7e27ba7a1750
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-07 15:42:36 +0500

    CoreModulesDone

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const app = require("./lib")
+// const app = require("./lib")
 
-const arr = [2,4,7,1,3,8,3];
+// const arr = [2,4,7,1,3,8,3];
 
-// console.log(arr[6])
-let result = arr.filter((item)=>{
-    // console.log(item)
-    return item>=4;
-})
+// // console.log(arr[6])
+// let result = arr.filter((item)=>{
+//     // console.log(item)
+//     return item>=4;
+// })
 
-console.log(result)
+// console.log(result)
 
-// console.log(app.z)
+// // console.log(app.z)
+
+
+const fs = require('fs')
+
+fs.writeFileSync("new.txt","Hello Sana Ulllah Rasheed")
+console.log("-->",__dirname)
+console.log("-->",__filename)
