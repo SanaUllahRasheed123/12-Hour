d74de03a6850602aa206acb654b7b60bba64101a
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-08 21:02:32 +0500

    data.js file created

--- index.js
+++ index.js
@@ -63,10 +63,11 @@
 /******************************also created a file with data.js ****************************************************************** */
 
 const http = require("http")
+const data = require('./data')
 
 http.createServer((req,res)=>{
     res.writeHead(404,{"Content-Type":'application\json'});
-    res.write(JSON.stringify({name:"SanaUllah",email:"sana@test.com" ,contact:"0322-4710317"}))
+    res.write(JSON.stringify(data))
     res.end();
 }).listen(5000)
 
