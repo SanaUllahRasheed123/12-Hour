589a8759bbf13913a8380329e5d821e614ab6ad9
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-08 21:07:12 +0500

    data.js file created and understanding

--- data.js
+++ data.js
@@ -1,7 +1,16 @@
-const data = [
-    {name:"SanaUllah",email:"sana@test.com" ,contact:"0322-4710317"},
-    {name:"UbaidUllah",email:"ubaid@test.com" ,contact:"031737297"},
-    {name:"Ahmad",email:"ahmad@test.com" ,contact:"03927794030247"}
-]
 
-module.exports=data;
+//if we want to use only one object use like this
+const data = {name:"ahmad", email:"ahamd@gmail.com", contact:"034747329799"}
+
+module.exports= data;
+
+//if we want to use more than one object use array of objects
+// const data = [
+//     {name:"SanaUllah",email:"sana@test.com" ,contact:"0322-4710317"},
+//     {name:"UbaidUllah",email:"ubaid@test.com" ,contact:"031737297"},
+//     {name:"Ahmad",email:"ahmad@test.com" ,contact:"03927794030247"}
+// ]
+
+// module.exports=data;
+
+
