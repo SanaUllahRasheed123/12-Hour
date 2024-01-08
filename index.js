727579717db47ff0f29b1ef9d3095b6cf5dd8737
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-08 23:00:41 +0500

    multiple helloFileCreated in files folder

--- index.js
+++ index.js
@@ -100,8 +100,20 @@
 
 
 const fs = require('fs')
+const path = require('path');
+const dirPath = path.join(__dirname,"files");
 
-fs.writeFileSync('hello.txt',"this is a hello file")
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(dirPath+"/hello"+i+".txt","this is a hello file")
+
+// }
+
+fs.readdir(dirPath,(err,files)=>{
+files.forEach((item)=>{
+    console.log(item)
+})
+})
+
 
 
 
