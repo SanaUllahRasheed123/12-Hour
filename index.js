99400bc204bb6a597ff031a7fac0216e12c7d33f
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-09 10:19:58 +0500

    handlingAsync done

--- index.js
+++ index.js
@@ -80,11 +80,19 @@
 let a = 10;
 let b = 0;
 
-setTimeout(()=>{
-    b=20;
-},2000)
 
+
+let waitingData = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(30)
+    },2000) 
+})
+
+waitingData.then((data)=>{
+b= data;
 console.log(a+b)
+
+})
 
 
 
