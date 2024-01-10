6b44a2042eefdfc770849f8d757dbd3c1c6776f3
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-10 14:52:22 +0500

    removed extension from url

--- index.js
+++ index.js
@@ -121,7 +121,18 @@ const app = express();
 const publicPath = path.join(__dirname,'public');
 
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get('',(_,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about',(_,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/help',(_,res)=>{
+    res.sendFile(`${publicPath}/help.html`)
+})
 app.listen(5000)
 
 
