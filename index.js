5fedd9690ef6cf3f218fc48faac3e79b43c5a768
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-12 23:42:52 +0500

    Alhamdulillah

--- index.js
+++ index.js
@@ -226,22 +226,33 @@
 
 // getData();
 
-const {MongoClient} = require('mongodb')
+// const {MongoClient} = require('mongodb')
+// const url = 'mongodb://127.0.0.1:27017';
+// const database = 'e-comm'
+// const client = new MongoClient(url);
+
+
+//  async function getData()
+// {
+//     let result =  await client.connect();
+//    let db= result.db(database);
+//    let collection = db.collection('products');
+//     let response = await collection.find({}).toArray();
+//    console.log(response)
+// }
+
+// getData();
+
+const {MongoClient} = require("mongodb");
 const url = 'mongodb://127.0.0.1:27017';
-const database = 'e-comm'
 const client = new MongoClient(url);
 
-
- async function getData()
-{
-    let result =  await client.connect();
-   let db= result.db(database);
-   let collection = db.collection('products');
-    let response = await collection.find({}).toArray();
-   console.log(response)
+async function main(){
+    await client.connect();
+    console.log("Connected Successfully to Server")
 }
 
-getData();
+main();
 
 
 
