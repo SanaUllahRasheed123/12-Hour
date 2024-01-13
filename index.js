02b4e5308091ff37d690d882bbbaf16a5a70795c
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-13 19:56:00 +0500

    Alhamdulillah

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ const client = new MongoClient(url);
     let result =  await client.connect();
    let db= result.db(database);
    let collection = db.collection('products');
-    let response = await collection.find({}).toArray();
+    let response = await collection.find({name:"Sana Ullah"}).toArray();
    console.log(response)
 }
 
