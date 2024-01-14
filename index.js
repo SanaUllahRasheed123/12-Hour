ed8157ccae4961a00a3a0148dfa44664c67b7884
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-14 16:55:38 +0500

    Read Data from MongoDb to Node js

--- index.js
+++ index.js
@@ -1,64 +1,20 @@
-// const { insert } = require('./insert');
+const {MongoClient} = require('mongodb')
+const url = 'mongodb://127.0.0.1:27017';
+const database = 'e-comm';
 
-const dbConnect = require('./mongodb')
+const client = new MongoClient(url);
+async function dbConnect()
+{
+    let result = await client.connect()
+    let db = result.db(database);
+    return db.collection('products');
+    // let response = await collection.find({}).toArray();
+    // console.log(response);
+}
 
 dbConnect().then((res)=>{
-    res.find({}).toArray().then((data)=>{
-        console.warn(data)
+    res.find().toArray().then((data)=>{
+        console.log(data)
     })
 })
-
-const main = async ()=>{
-    // console.log("main function called")
-    let data = await dbConnect();
-    data = await data.find().toArray();
-    console.warn(data)
-}
-main();
-
-
-
-// index.js
-
-// const { dbConnect } = require('./mongodb');
-
-// const main = async () => {
-//     try {
-//         const collection = await dbConnect();
-
-//         // Display all data in the 'products' collection
-//         const data = await collection.find({}).toArray();
-//         console.warn(data);
-//     } catch (error) {
-//         console.error('Error in main function:', error);
-//     }
-// }
-
-// main();
-
-
-    const collection = db.collection('products')
-    const findResult = await collection.find().toArray();
-    console.log(findResult)
-
-    
-    
-
-
-main();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(dbConnect());
