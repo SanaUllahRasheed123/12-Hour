64f6ae1b46aac33ca097d77ffdddf9be5f98ee28
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-14 17:09:05 +0500

    Read Data from MongoDb to Node js by creating another file mongodb.js and creating two functions in mongodb.js

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const {MongoClient} = require('mongodb')
-const url = 'mongodb://127.0.0.1:27017';
-const database = 'e-comm';
 
-const client = new MongoClient(url);
-async function dbConnect()
-{
-    let result = await client.connect()
-    let db = result.db(database);
-    return db.collection('products');
-    // let response = await collection.find({}).toArray();
-    // console.log(response);
-}
+const dbConnect = require('./mongodb')
 
 // dbConnect().then((res)=>{
 //     res.find().toArray().then((data)=>{
 //         console.log(data)
 //     })
 // })
-// console.log(dbConnect());
 
 const main = async () =>{
     // console.log("main function called");
@@ -25,5 +13,4 @@ const main = async () =>{
     data = await data.find().toArray();
     console.warn(data)
 }
-
 main();
