e343aee1798c15eb6aca3f86eeab38dc68b30fee
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-14 17:03:17 +0500

    Read Data from MongoDb to Node js

--- index.js
+++ index.js
@@ -12,9 +12,18 @@ async function dbConnect()
     // console.log(response);
 }
 
-dbConnect().then((res)=>{
-    res.find().toArray().then((data)=>{
-        console.log(data)
-    })
-})
+// dbConnect().then((res)=>{
+//     res.find().toArray().then((data)=>{
+//         console.log(data)
+//     })
+// })
 // console.log(dbConnect());
+
+const main = async () =>{
+    // console.log("main function called");
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.warn(data)
+}
+
+main();
