cdb5424d91130c59f668caaa90eccae1c2e91600
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-18 23:59:13 +0500

    firt api done with get post methode

--- api.js
+++ api.js
@@ -2,11 +2,18 @@ const dbConnect = require('./mongodb');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', async(req,res)=>{
     let data = await dbConnect();
     data = await data.find().toArray()
     // console.log(data)
     res.send(data)
 });
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    res.send({name:'Sana Ullah'})
+})
 
 app.listen(5000)
