bacc7b95244b0c159b77b42375727f7dd31f0ae1
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-21 15:37:33 +0500

    mongodb done

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const dbConnect = require('./mongodb');
+const mongodb = require('mongodb')
 const express = require('express');
 const app = express();
 
@@ -27,6 +28,13 @@ let result = await data.updateOne(
     { $set :req.body}
 )
     res.send({result:"update"})
+})
+
+app.delete("/:id",async(req,res)=>{
+    console.log(req.params.id)
+    const data = await dbConnect();
+    const result = await data.deleteOne({_id: new mongodb.ObjectId(req.params.id)})
+    res.send(result)
 })
 
 app.listen(5000)
