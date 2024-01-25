eec8a5a1033693ed77812fd5ef59fc4aed56c652
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-25 23:17:27 +0500

    multer program

--- index.js
+++ index.js
@@ -446,7 +446,19 @@ const express = require('express')
 const multer = require('multer');
 const app = express();
 
-
+const upload = multer({
+    storage:multer.diskStorage({
+        destination:function(req,file,cb)
+        {
+            cb(null,'uploads')
+        },
+        filename:function(req,file,cb)
+        {
+            cb(null,file.fieldname+"-"+Date.now()+",JPG")
+        }
+    })
+   
+}).single("user_file")
 app.post('/upload',(req,res)=>{
     res.send("file upload")
 });
