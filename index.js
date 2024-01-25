7981c2cf907320647119bb98ab7f77e5606bf33d
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-25 22:25:32 +0500

    multer program

--- index.js
+++ index.js
@@ -439,3 +439,17 @@
 
 // app.listen(5000);
 
+
+//multer done
+
+const express = require('express')
+const multer = require('multer');
+const app = express();
+
+
+app.post('/upload',(req,res)=>{
+    res.send("file upload")
+});
+
+app.listen(5000)
+
