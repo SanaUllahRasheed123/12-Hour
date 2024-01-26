20fd8a6010507256136dd873b98a55db05020306
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-26 13:55:39 +0500

    event and event emmiter done

--- index.js
+++ index.js
@@ -481,3 +481,40 @@
 
 //Events and Event Emitter in Node Js
 
+const express = require('express');
+
+const EventEmitter = require('events');
+const app = express()
+
+const event = new EventEmitter();
+
+
+let count =0;
+
+event.on('countAPI',()=>{
+    count++;
+    console.log('event called',count)
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("api called")
+    event.emit('countAPI');
+})
+
+app.get('/search',(req,res)=>{
+    res.send("search api called")
+    event.emit('countAPI');
+
+})
+
+app.get('/update',(req,res)=>{
+    res.send("update api called")
+    event.emit('countAPI');
+
+})
+
+
+
+app.listen(5000)
+
